8612391e703772916799c799351daa83c06f0f1a
kurtov <kurtov@unact.ru>
2018-03-23 15:34:04 +0300

    DEV-34793

--- lib/cat-features.rb
+++ lib/cat-features.rb
@@ -1,13 +1,14 @@
 require "active_record"
+require "acts_as_singleton"
 require "composite_primary_keys"
 require "cat-features/version"
 require "cat-features/id_generator"
 require "cat-features/extrable"
 require "cat-features/user_option"
-require 'cat-features/active_record/base'
-require 'cat-features/active_record/relation'
-require 'cat-features/active_record/persistance'
-
+require "cat-features/active_record/base"
+require "cat-features/active_record/relation"
+require "cat-features/active_record/persistance"
+require "cat-features/dummy"
 
 module CatFeatures
 end
